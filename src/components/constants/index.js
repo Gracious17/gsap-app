b5b3f67247ea46335266881b799a00a4879b02e4
Gracious17 <kingsleygracious16@gmail.com>
2025-10-21 10:33:34 +0100

    first commit

---
+++ src/components/constants/index.js
@@ -0,0 +1,10 @@
+export const navlinks=[
+    
+            { label: "Store" },
+            { label: "Mac" },
+            { label: "iphone" },
+            { label: "Watch" },
+            { label: "Vision" },
+            { label: "Airpod" },
+        
+]
