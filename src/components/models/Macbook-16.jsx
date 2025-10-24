64b9f08be7cdb36ccf55d87db975f9e5b3b57fcd
Gracious17 <kingsleygracious16@gmail.com>
2025-10-24 01:04:10 +0100

    adjusted  rgb colors

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -11,7 +11,7 @@ Title: macbook pro M3 16 inch 2024
 import React, { useEffect } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 import useMacbookStore from "../../store";
-import { Color } from "three";
+import { Color, SRGBColorSpace } from "three";
 import { noChangeParts } from "../constants";
 
 export default function MacbookModel16(props) {
@@ -20,6 +20,8 @@ export default function MacbookModel16(props) {
 
 
   const texture = useTexture("/screen.png");
+   texture.colorSpace = SRGBColorSpace;
+    texture.needsUpdate = true;
 
   useEffect(() => {
     scene.traverse((child) => {
@@ -121,7 +123,6 @@ export default function MacbookModel16(props) {
       />
       <mesh
         geometry={nodes.Object_123.geometry}
-        material={materials.sfCQkHOWyrsLmor}
         rotation={[Math.PI / 2, 0, 0]}
       >
         <meshBasicMaterial map={texture} />
